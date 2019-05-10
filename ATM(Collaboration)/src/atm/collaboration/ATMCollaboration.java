ccbb76cda9dbcd96f4e1a5cd88e8a5929e87a53d
Ananda Bayu <anandabayuf@fakfak.com>
2019-05-10 10:17:46 +0700

    Revert "ngosongin branch master"
    This reverts commit 74c9cd4378bd3af287d6a9852dc07499945a835d.

---
+++ ATM(Collaboration)/src/atm/collaboration/ATMCollaboration.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm.collaboration;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ATMCollaboration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.print("Hello Nadhilah");   
+
+        System.out.print("Hello Bayu");
+
+        System.out.print("Hello World");
+        System.out.print(2019);
+
+    }
+    
+}
